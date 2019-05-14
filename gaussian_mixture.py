f2e3578c034689276ea13952354b0e6e5f831c62
Miguel-Fuentes <Miguel.Fuentes.new@gmail.com>
2019-05-14 16:03:36 -0400

    Added keyword agrument for returning log likelihood

--- gaussian_mixture.py
+++ gaussian_mixture.py
@@ -6,7 +6,7 @@ class gaussian_mix:
     def __init__(self, k):
         self.K = k
     
-    def train(self, data, num_iters, fixed_var=False):
+    def train(self, data, num_iters, fixed_var=False, return_likelihood=False):
         if fixed_var:
             self.params = list(np.random.uniform(low=np.min(data), high=np.max(data), size=self.K))
         else: 
@@ -15,32 +15,39 @@ class gaussian_mix:
                 list(np.random.uniform(low=np.std(data, ddof=1)/2, high=np.std(data, ddof=1)*2, size=self.K))
             ))
 
-        self.weights = np.random.uniform(size=self.K)
-        self.weights /= np.sum(self.weights)
-
-        for _ in range(num_iters):
-            self.probs = []
+        self.bias = np.random.uniform(size=self.K)
+        self.bias /= np.sum(self.bias)
+        
+        iters = 0
+        while iters <= num_iters:
+            iters += 1
+            weights = []
             if fixed_var:
                 for mean in self.params:
-                    self.probs.append(norm.pdf(data, loc=mean, scale=1.0))
+                    weights.append(norm.pdf(data, loc=mean, scale=1.0))
             else:
                 for mean, var in self.params:
-                    self.probs.append(norm.pdf(data, loc=mean, scale=sqrt(var)))
+                    weights.append(norm.pdf(data, loc=mean, scale=sqrt(var)))
 
-            self.probs = np.hstack(self.probs) * self.weights
-            self.probs /= np.sum(self.probs, axis=1, keepdims=True)
+            if iters == num_iters and return_likelihood:
+                probs = np.hstack(weights)
+                
+            weights = np.hstack(weights) * self.bias
+            weights /= np.sum(weights, axis=1, keepdims=True)
 
-            n = np.sum(self.probs, axis=0) 
-            self.weights = n / data.shape[0]
+            n = np.sum(weights, axis=0) 
+            self.bias = n / data.shape[0]
 
-            means = np.sum(self.probs * data, axis=0) / n
+            means = np.sum(weights * data, axis=0) / n
             if fixed_var:
                 self.params = list(means)
             else:
-                stdevs = np.sum(self.probs * ((np.repeat(data, means.shape[0], axis=1) - means) ** 2), axis=0) / n
+                stdevs = np.sum(weights * ((np.repeat(data, means.shape[0], axis=1) - means) ** 2), axis=0) / n
                 self.params = list(zip(
                     list(means),
                     list(stdevs)
                 ))
-    def calc_likelihood(self, data):
-        pass
+                
+            if iters == num_iters and return_likelihood:
+                weighted_probs = np.sum(weights * probs, axis=1)
+                return np.sum(np.log(weighted_probs))
